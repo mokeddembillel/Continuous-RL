80d4d32d5a2e676cb257b2d614c39402e04ad419
Billel Mokeddem <billel.mokeddem.ml@gmail.com>
2021-06-25 16:25:50 +0100

    Adding the code to run and visualize SAC's Q contours on LQR 2D

--- Algorithms/SAC/main_sac.py
+++ Algorithms/SAC/main_sac.py
@@ -3,7 +3,6 @@ import gym
 import numpy as np
 import pybulletgym
 from sac_torch import Agent
-from utils import plot_learning_curve
 from gym import wrappers
 import math
 import gym_lqr
@@ -11,13 +10,16 @@ import gym_lqr
 
 if __name__ == '__main__':
     #env = gym.make('gym_lqr:lqr-stochastic-v0')
-    env = gym.make('gym_lqr:lqr-v0')
+    #env = gym.make('gym_lqr:lqr-2d-v0')
+    #env = gym.make('gym_lqr:lqr-v0')
     #env = gym.make('InvertedPendulumPyBulletEnv-v0')
     #env = gym.make('InvertedPendulum-v2')
+    #env = gym.make('Walker2DPyBulletEnv-v0')
+    env = gym.make('Ant-v2')
     #print(env.action_space.shape[0])
     agent = Agent(input_dims=env.observation_space.shape, env=env,
             n_actions=env.action_space.shape[0])
-    n_games = 3000
+    n_games = 3000 
     # uncomment this line and do a mkdir tmp && mkdir video if you want to
     # record video of the agent playing the game.
     #env = wrappers.Monitor(env, 'tmp/video', video_callable=lambda episode_id: True, force=True)
@@ -38,17 +40,17 @@ if __name__ == '__main__':
     for i in range(n_games):
         
         #observation = env.reset(init_x=np.array([7., 7., 5.]), max_steps=200)
-        observation = env.reset(init_x=np.array([100]), max_steps=15)
-        #observation = env.reset()
+        #observation = env.reset(init_x=np.array([100, 100]), max_steps=200)
+        observation = env.reset()
 
         #print(observation)
         done = False
         score = 0
         while not done:
-            env.render()
-            print('state: ', np.squeeze(observation))
+            #env.render()
+            #print('state: ', np.squeeze(observation))
             action = agent.choose_action(observation)
-            print('ac: ', np.squeeze(action))
+            #print('ac: ', np.squeeze(action))
             observation_, reward, done, info = env.step(action)
             #print('re:', reward)
             #print('Q: ', np.squeeze(env.get_Q()))
@@ -57,7 +59,7 @@ if __name__ == '__main__':
             if not load_checkpoint:
                 agent.learn()
             observation = observation_
-        score = score / 200
+        score = score
         score_history.append(score)
         avg_score = np.mean(score_history[-20:])
         
@@ -65,8 +67,8 @@ if __name__ == '__main__':
             best_score = avg_score
             if not load_checkpoint:
                 agent.save_models()
-                print('P: ', env.get_P())
-                np.save('tmp/sac/optimal_P', env.get_P())
+                #print('P: ', env.get_P())
+                #np.save('tmp/sac/optimal_P', env.get_P())
                 
                 
         print('episode ', i, 'score %.1f' % score, 'avg_score %.1f' % avg_score)
