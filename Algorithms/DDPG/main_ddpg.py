ced59972fb402fb49c7042d81b0d07d165aa1a61
Billel Mokeddem <billel.mokeddem.ml@gmail.com>
2021-06-25 23:13:14 +0100

    Add DDPG implementation from Dr.phil

---
+++ Algorithms/DDPG/main_ddpg.py
@@ -0,0 +1,43 @@
+from ddpg_torch import Agent
+import gym
+import numpy as np
+from utils import plotLearning
+
+
+#del gym.registry.env_specs['gym_lqr:lqr-v0']
+import gym_lqr
+
+#env = gym.make('LunarLanderContinuous-v2')
+env = gym.make('gym_lqr:lqr-v0')
+agent = Agent(alpha=0.000025, beta=0.00025, input_dims=[8], tau=0.001, env=env,
+              batch_size=64,  layer1_size=400, layer2_size=300, n_actions=2)
+
+#agent.load_models()
+np.random.seed(0)
+
+score_history = []
+for i in range(1000):
+    obs = env.reset()
+    done = False
+    score = 0
+    while not done:
+        act = agent.choose_action(obs)
+        new_state, reward, done, info = env.step(act)
+        print('state: ', np.squeeze(obs))
+        print('ac: ', np.squeeze(act))
+        print('re:', reward)
+        agent.remember(obs, act, reward, new_state, int(done))
+        agent.learn()
+        score += reward
+        obs = new_state
+        #env.render()
+    score_history.append(score)
+
+    #if i % 25 == 0:
+    #    agent.save_models()
+
+    print('episode ', i, 'score %.2f' % score,
+          'trailing 100 games avg %.3f' % np.mean(score_history[-100:]))
+
+filename = 'LunarLander-alpha000025-beta00025-400-300.png'
+plotLearning(score_history, filename, window=100)
---
+++ Algorithms/DDPG/utils.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+
+class OUActionNoise(object):
+    def __init__(self, mu, sigma=0.15, theta=.2, dt=1e-2, x0=None):
+        self.theta = theta
+        self.mu = mu
+        self.sigma = sigma
+        self.dt = dt
+        self.x0 = x0
+        self.reset()
+
+    def __call__(self):
+        x = self.x_prev + self.theta * (self.mu - self.x_prev) * self.dt + \
+            self.sigma * np.sqrt(self.dt) * np.random.normal(size=self.mu.shape)
+        self.x_prev = x
+        return x
+
+    def reset(self):
+        self.x_prev = self.x0 if self.x0 is not None else np.zeros_like(self.mu)
+
+    def __repr__(self):
+        return 'OrnsteinUhlenbeckActionNoise(mu={}, sigma={})'.format(
+                                                            self.mu, self.sigma)
+
+def plotLearning(scores, filename, x=None, window=5):   
+    N = len(scores)
+    running_avg = np.empty(N)
+    for t in range(N):
+	    running_avg[t] = np.mean(scores[max(0, t-window):(t+1)])
+    if x is None:
+        x = [i for i in range(N)]
+    plt.ylabel('Score')       
+    plt.xlabel('Game')                     
+    plt.plot(x, running_avg)
+    plt.savefig(filename)
